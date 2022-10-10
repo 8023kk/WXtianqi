3345b76447983c4a05da7861e547b6dac755ae23
8023kk <88925708+8023kk@users.noreply.github.com>
2022-10-10 23:31:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ def get_ciba():
     return note_ch, note_en
  
  
-def send_message(to_user, access_token, name, region_name, weather, temp, wind_dir, note_ch, note_en):
+def send_message(to_user, access_token, region_name, weather, temp, wind_dir, note_ch, note_en):
     url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token={}".format(access_token)
     week_list = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
     year = localtime().tm_year
@@ -141,10 +141,6 @@ def send_message(to_user, access_token, name, region_name, weather, temp, wind_d
         "url": "http://weixin.qq.com/download",
         "topcolor": "#FF0000",
         "data": {
-            "name": {
-                "value": name,
-                "color": get_color()
-            },
             "date": {
                 "value": "{} {}".format(today, week),
                 "color": get_color()
@@ -234,6 +230,6 @@ if __name__ == "__main__":
         note_ch, note_en = get_ciba()
     # 公众号推送消息
     for user in users:
-        send_message(user, accessToken, name, region, weather, temp, wind_dir, note_ch, note_en)
+        send_message(user, accessToken, region, weather, temp, wind_dir, note_ch, note_en)
     os.system("pause")
 
