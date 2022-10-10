c75f28d95b8958fe61ce3f53de518385f9492828
8023kk <88925708+8023kk@users.noreply.github.com>
2022-10-10 13:15:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -141,6 +141,10 @@ def send_message(to_user, access_token, region_name, weather, temp, wind_dir, no
         "url": "http://weixin.qq.com/download",
         "topcolor": "#FF0000",
         "data": {
+            "name": {
+                "value": name,
+                "color": get_color()
+            },
             "date": {
                 "value": "{} {}".format(today, week),
                 "color": get_color()
@@ -229,5 +233,6 @@ if __name__ == "__main__":
         note_ch, note_en = get_ciba()
     # 公众号推送消息
     for user in users:
-        send_message(user, accessToken, region, weather, temp, wind_dir, note_ch, note_en)
+        send_message(user, accessToken, name, region, weather, temp, wind_dir, note_ch, note_en)
     os.system("pause")
+
